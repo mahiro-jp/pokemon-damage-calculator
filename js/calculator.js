625939d6c0a9300d3d809460270c1128b4174791
mahiro-jp <mahiro.games.fun@gmail.com>
2026-04-11 23:36:20 +0900

    初期コミット

---
+++ js/calculator.js
@@ -0,0 +1,6 @@
+// ダメージ計算のメインロジックをここに記述します
+function calculateDamage(stats) {
+    console.log("計算を開始します...");
+    // TODO: 実際のダメージ計算式を実装
+    return 0;
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("アプリが起動しました");
+});
